8aa4e13aa3c7ce73ee161c19729985c06a55ef60
fqueta <ger.maisaqui1@gmail.com>
2024-02-04 23:43:44 -0300

    04/02/2024 02

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'version' => '1.0.0.1',
+    'version' => '1.0.0.2',
     'name' => env('APP_NAME', 'Designações vida e ministério'),
 
     /*
